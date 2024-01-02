a794dd1de6e60b024957053d6b46b293302d4b79
Matheus de Mello Guerra <matheusguerra174@gmail.com>
2024-01-02 10:31:51 -0300

    dont convert json file

--- cs_integration/api/google_api/scripts/autentication.py
+++ cs_integration/api/google_api/scripts/autentication.py
@@ -4,9 +4,7 @@ import os
 import json
 
 # Carregue as credenciais diretamente do JSON
-credentials_json = os.environ['CREDENTIAL']
-
-credentials_data = json.loads(credentials_json)
+credentials_data = os.environ['CREDENTIAL']
 
 sheet_id = os.environ['SHEET_ID']
 
