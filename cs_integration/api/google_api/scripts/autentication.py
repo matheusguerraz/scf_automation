caf47497935493aab519f03a6db4e6f795d8eebf
Matheus de Mello Guerra <matheusguerra174@gmail.com>
2024-01-02 11:21:41 -0300

    credentials function

--- cs_integration/api/google_api/scripts/autentication.py
+++ cs_integration/api/google_api/scripts/autentication.py
@@ -12,16 +12,17 @@ credentials_data = json.loads(credentials_json)
 
 sheet_id = os.environ['SHEET_ID']
 
-def load_credentials(credentials_data):
+def load_credentials(credentials_json):
     try:
+        credentials_data = json.loads(credentials_json)
         creds = service_account.Credentials.from_service_account_info(
             credentials_data,
             scopes=['https://www.googleapis.com/auth/spreadsheets']
         )
-        print("Credenciais carregadas com sucesso.")
         return creds
-    except exceptions.GoogleAuthError as e:
-        print(f"Ocorreu um erro ao carregar as credenciais: {e}")
+    except Exception as e:
+        print(f'Ocorreu um erro ao carregar as credenciais: {e}')
+        return None
 
 
 def authorize_client(credentials_data):
